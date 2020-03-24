ea4c2b264ceedeb381d8c599484c3bc188171397
Kuba Sekowski <sequba@gmail.com>
2020-03-24 19:31:40 +0100

    Create repos list component

---
+++ src/app/repos/repos-list/repos-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rex-repos-list',
+  template: `
+    <p>
+      repos-list works!
+    </p>
+  `,
+  styles: []
+})
+export class ReposListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
