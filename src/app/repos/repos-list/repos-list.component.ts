b0b20fa3cd3de8b685f2c198aca2bbf9538e5429
Kuba Sekowski <sequba@gmail.com>
2020-03-25 20:52:47 +0100

    Display message when there is no repositories for this user

--- src/app/repos/repos-list/repos-list.component.ts
+++ src/app/repos/repos-list/repos-list.component.ts
@@ -4,11 +4,14 @@ import { Repo } from 'src/app/dtos/repo';
 @Component({
   selector: 'rex-repos-list',
   template: `
-    <ul>
-      <li *ngFor="let repo of repos">
-        <rex-repos-list-item [repo]="repo"></rex-repos-list-item>
-      </li>
-    </ul>
+    <ng-container *ngIf="repos">
+      <p *ngIf="repos.length <= 0"> Found no repositories owned by this user </p>
+      <ul>
+        <li *ngFor="let repo of repos">
+          <rex-repos-list-item [repo]="repo"></rex-repos-list-item>
+        </li>
+      </ul>
+    </ng-container>
   `,
   styles: []
 })
