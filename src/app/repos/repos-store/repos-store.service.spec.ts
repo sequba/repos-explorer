eff790db90fc058063407d2ae3d36de62b461208
Kuba Sekowski <sequba@gmail.com>
2020-03-25 18:52:36 +0100

    Tests for repos store

--- src/app/repos/repos-store/repos-store.service.spec.ts
+++ src/app/repos/repos-store/repos-store.service.spec.ts
@@ -3,12 +3,19 @@ import { TestBed } from '@angular/core/testing';
 import { ReposStoreService } from './repos-store.service';
 import { ReposApiService } from 'src/app/github-api/repos-api/repos-api.service';
 import { of } from 'rxjs';
+import { Repo } from 'src/app/dtos/repo';
 
 describe('ReposStoreService', () => {
   let service: ReposStoreService;
 
+  const reposMock: Repo[] = [
+    {name: 'repo1', fullName: 'repo1', owner: 'user', isFork: false},
+    {name: 'repo2', fullName: 'repo2', owner: 'user', isFork: false},
+    {name: 'fork', fullName: 'fork', owner: 'user', isFork: true}
+  ];
+
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [{provide: ReposApiService, useValue: {fetchReposByUser$: () => of([])}}]});
+    TestBed.configureTestingModule({providers: [{provide: ReposApiService, useValue: {fetchReposByUser$: () => of(reposMock)}}]});
     service = TestBed.inject(ReposStoreService);
   });
 
@@ -19,7 +26,7 @@ describe('ReposStoreService', () => {
   describe('getNonForkReposByUser$', () => {
     it('should return only non-fork repos', (done) => {
       service.getNonForkReposByUser$('testuser').subscribe(repos => {
-        fail();
+        expect(repos.length).toEqual(2);
         done();
       });
     });
