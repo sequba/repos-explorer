c43f27ff6746dbfbaf767265d16927ff70af3f02
Kuba Sekowski <sequba@gmail.com>
2020-04-13 15:57:28 +0200

    Add spinner to repos search

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ReposStoreService } from './repos-store/repos-store.service';
-import { Observable, NEVER } from 'rxjs';
+import { Observable, NEVER, BehaviorSubject } from 'rxjs';
 import { Repo } from '../dtos/repo';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, share, mapTo, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rex-repos',
   template: `
-    <div class="container">
-      <div class="row p-3 my-3 bg-primary text-white">
+    <div class="container bg-white">
+      <div class="row card p-3 my-3 bg-primary text-white">
         <rex-repos-search (username)="showRepos($event)"></rex-repos-search>
       </div>
-
-      <div *ngIf="errorMessage" class="card bg-warning">
-        <p>{{ errorMessage }}</p>
+      <div *ngIf="errorMessage" class="alert alert-dark">
+        {{ errorMessage }}
+      </div>
+      <div *ngIf="isLoading | async" class="row d-flex justify-content-center">
+        <div class="row spinner-border text-primary"></div>
       </div>
       <rex-repos-list [repos]="repos | async" class="row"></rex-repos-list>
     </div>
@@ -23,6 +25,7 @@ import { map, catchError } from 'rxjs/operators';
 export class ReposComponent implements OnInit {
   repos: Observable<Repo[]> = NEVER;
   errorMessage: string | null = null;
+  isLoading = new BehaviorSubject<boolean>(false);
 
   readonly userNotFoundMsg = 'User not found';
   readonly genericErrorMsg = 'Fetching data from Github failed';
@@ -31,11 +34,15 @@ export class ReposComponent implements OnInit {
 
   showRepos(user: string): void {
     this.errorMessage = null;
+    this.isLoading.next(true);
 
-    this.repos = this.reposStore.getNonForkReposByUser$(user).pipe(catchError(err => {
-      this.errorMessage = (err.status === 404) ? this.userNotFoundMsg : this.genericErrorMsg;
-      return NEVER;
-    }));
+    this.repos = this.reposStore.getNonForkReposByUser$(user).pipe(
+      catchError(err => {
+        this.errorMessage = (err.status === 404) ? this.userNotFoundMsg : this.genericErrorMsg;
+        return [];
+      }),
+      tap(() => this.isLoading.next(false))
+    );
   }
 
   ngOnInit(): void {
