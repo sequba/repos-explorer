93558b8c7b2656334073e0e2b19508125f54f3fb
Kuba Sekowski <sequba@gmail.com>
2020-04-13 15:55:47 +0200

    Add image to the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rex-root',
   template: `
-    <div class="jumbotron text-center">
+    <div class="jumbotron jumbotron-fluid text-center">
       <h1>ReposExplorer</h1>
     </div>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [`
+    .jumbotron {
+      background-image: url("assets/img/header.jpg");
+      background-size: cover;
+      background-position: 50% 30%;
+    }
+  `]
 })
 export class AppComponent {
   title = 'repos-explorer';
